81106f7cf0fc5aa097e27a015be352d0d72e4349
Keir Innes <keir.innes@gmail.com>
2023-12-07 09:22:45 +1300

    Adding small changes plus RTL tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 import { describe, it, expect } from "vitest";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, MemoryRouter } from "react-router-dom";
+import HomePage from "./Components/Home-Page/HomePage.jsx";
+import Shop from "./Components/Shop-Page/Shop.jsx";
+import Cart from "./Components/Cart-Page/Cart.jsx";
 
 describe("App component", () => {
   it("renders the navigation bar with Home and Shop links", () => {
@@ -15,14 +18,37 @@ describe("App component", () => {
     expect(screen.getByText(/Cart.*0.*/)).toBeInTheDocument(); //Cart (0)
   });
 
-  /* NEED TO WORK OUT HOW TO DO THIS
   it("renders the home page", () => {
     render(
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     );
     expect(screen.getByText("This is the home page")).toBeInTheDocument();
   });
-  */
+
+  it("navigates to the Cart page when Cart link is clicked", () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+
+    // Simulate a click on the Cart link in the navigation bar
+    const cartLink = screen.getByText(/Cart.*0.*/);
+    fireEvent.click(cartLink);
+
+    // Check if the Cart component is rendered
+    expect(screen.getByText("This is your cart:")).toBeInTheDocument();
+  });
 });
